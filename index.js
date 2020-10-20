885c8ad50f2d0b375e00eb2bda14e6f5f8b8c9bd
Augusto Almeida <jat3099@hotmail.com>
2020-10-20 13:43:33 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+//set handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const stuff = "Hello welcome to my stock web app";
+//set handlerbar routes
+app.get('/', function (req, res) {
+    res.render('home', { stuff});
+});
+
+//linking the html file with the app
+//this is the route for the static file ->file that dont change
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => { console.log(`Server is listening at :${PORT}`) });
