39c15734c3cae12164b3a2626160d922e91ba5c6
Augusto Almeida <jat3099@hotmail.com>
2020-11-02 11:50:17 -0500

    Final update

--- index.js
+++ index.js
@@ -3,26 +3,62 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
-
+const bodyParser = require('body-parser');
+const request = require('request');
 const PORT = process.env.PORT || 5000;
-//set handlebars middleware
+
+ 
+
 app.engine('handlebars', exphbs() );
 app.set('view engine', 'handlebars');
+
 
 //web comment
 const stuff = "Hello welcome to my stock web app";
 
+//APi token pk_f434330ba373450585672d1c4b21a153
+
+//set handlebars middleware
+app.use(bodyParser.urlencoded({extended: false})); 
+//call API function
+function call_api(finishedApi, ticker){
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_f434330ba373450585672d1c4b21a153', {json: true}, (err,res, body) => {
+	if(err)
+		{console.log(err);}
+	if(res.statusCode === 200);
+	
+	finishedApi(body);
+
+});
+};
+
+//set hanlderbar index get route
 //set handlerbar routes
 app.get('/', function (req, res) {
-    res.render('home', { stuff});
+    call_api(function(apiData) {
+    	res.render('home', { 
+    	stock: apiData
+    });
+    });
+    
 });
 
-app.get('/about.html', function (req,res) { res.render('about');
+//set handlerbar index post route
+app.post('/', function(req,res) {
+	call_api(function(apiData){
+		//posted = req.body.ticker
+		res.render('home',{
+			stock: apiData,
+			
+		});
+	}, req.body.ticker);
 });
 
-app.get('/about.html', function (req, res) {
-    res.render('about');
+app.get('/about.html', function (req,res)
+ { res.render('about');
 });
+
+ 
 
 //linking the html file with the app
 //this is the route for the static file ->file that dont change
